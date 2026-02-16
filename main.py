6351c70b7c9d720a072e68f78a3c8c739c8dfa45
lmabbutt <lmabbutt@asu.edu>
2026-02-16 11:12:03 -0700

    diddy

--- main.py
+++ main.py
@@ -12,16 +12,16 @@ while True:
         my_oled.print_text("test",0,0)
     
     if state == 1:
-        my_oled.print_text("something else",0,0)
+        my_oled.print_text("something else",0,50)
     
     if state == 2:
         my_oled.oled.fill(0)
-        my_oled.oled.line(0,0,10,10,1)
+        my_oled.oled.line(0,64,128,0,1)
         my_oled.oled.show()
     
     if state == 3:
         my_oled.oled.fill(0)
-        my_oled.graphics.fill_rect(0,0,10,10,1)
+        my_oled.graphics.fill_rect(64,32,128,64,1)
         my_oled.oled.show()
 
 
