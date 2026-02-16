5b0fc12fd5163eaef37d5115524a329bbf3963df
lmabbutt <lmabbutt@asu.edu>
2026-02-16 12:20:20 -0700

    chaged

--- my_oled.py
+++ my_oled.py
@@ -3,7 +3,7 @@ import ssd1306
 import gfx
 
 # Initialize I2C and OLED
-i2c = SoftI2C(scl=Pin(21), sda=Pin(22))
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
 oled_width = 128
 oled_height = 64
 oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
